c61155c81f9a79f6ff6d8e68dabbfb07896505a7
Ashish Chawda <ashish11chawda@gmail.com>
2022-08-14 11:39:01 +0530

    initial commit

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,6 @@
 // call quotes api to get a list of quotes
-let url = "https://quotesapibitd.herokuapp.com/quotes";
+// let url = "https://quotesapibitd.herokuapp.com/quotes";
+let url = "http://localhost:5000/quotes";
 async function getQuotes() {
     const quotes = document.querySelector('#quotes');
     const quote = document.querySelector('#quote');
@@ -84,5 +85,73 @@ async function createQuote() {
         }
     } else {
         alert("Please fill all the fields");
+    }
+}
+
+async function updateQuote() {
+    const quote = document.querySelector('#quote');
+    const author = document.querySelector('#author');
+    const quoteData = document.querySelector('#quoteData');
+    const authorData = document.querySelector('#authorData');
+    const quoteId = document.querySelector('#quoteId');
+
+    if (quote.value && author.value) {
+        const response = await fetch(`${url}/${quoteId.value}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                quote: quote.value,
+                author: author.value
+            })
+        });
+        if (response.status === 404) {
+            quoteData.innerHTML = "No quote found";
+            authorData.innerHTML = "Try with different ID";
+        }
+        else if (response.status == 500) {
+            quoteData.innerHTML = "Internal Server Error";
+            authorData.innerHTML = "Problem saving quote";
+        }
+        else {
+            let data = await response.json();
+            console.log(data.quote);
+            if (data.quote && data.author) {
+                console.log(data.author);
+                console.log(quote.innerHTML);
+                quoteData.innerHTML = `${data.quote}`;
+                authorData.innerHTML = `${data.author}`;
+                
+            } else {
+                quoteData.innerHTML = "No quote found";
+                authorData.innerHTML = "";
+            }
+        }
+    } else {
+        alert("Please fill all the fields");
+    }
+}
+
+async function deleteQuote() {
+    const quoteId = document.querySelector('#quoteId');
+
+    if(quoteId.value) {
+        const response = await fetch(`${url}/${quoteId.value}`, {
+            method: 'DELETE'
+        });
+        if (response == null || response == "" || response.status === 404) {
+            quote.innerHTML = "No quote found";
+            author.innerHTML = "Try with different ID";
+        } else {
+            let data = await response.json();
+            console.log(data);
+            if (data && data.quote && data.author) {
+                quote.innerHTML = data.quote;
+                author.innerHTML = data.author;
+            } else {
+                quote.innerHTML = "No quote found";
+            }
+        }
     }
 }
